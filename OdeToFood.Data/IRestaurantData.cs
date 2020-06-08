3704ab860cead5ca879c7bca09d5be0fb35e2fbb
mjbryan <mjbyran10@gmail.com>
2020-06-08 13:26:26 +0200

    adding post requests

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -10,7 +10,8 @@ namespace OdeToFood.Data
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
-
+        Restaurant Update(Restaurant updatedRestaurant);
+        int Commit();
     }
     public class InMemoryRestaurantData : IRestaurantData
     {
@@ -25,6 +26,21 @@ namespace OdeToFood.Data
             };
         }
 
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if(restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+        public int Commit()
+        {
+            return 0;
+        }
         public Restaurant GetById(int id)
         {
             return restaurants.SingleOrDefault(r => r.Id == id);
